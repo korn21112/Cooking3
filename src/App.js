4f1b4eb7c64d853f9fd0e11c379bb81dc9f0707d
attakorn <attakorn21112@gmail.com>
2021-10-08 14:57:21 +0700

    create edit profile page

--- src/App.js
+++ src/App.js
@@ -1,96 +1,3 @@
-// /**
-//  * Sample React Native App
-//  * https://github.com/facebook/react-native
-//  *
-//  * @format
-//  * @flow strict-local
-//  */
-
-// import React from 'react';
-// import {
-//   SafeAreaView,
-//   ScrollView,
-//   StatusBar,
-//   StyleSheet,
-//   Text,
-//   useColorScheme,
-//   View,
-// } from 'react-native';
-
-// import {
-//   Colors,
-//   DebugInstructions,
-//   Header,
-//   LearnMoreLinks,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
-
-// import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
-// import firestore from '@react-native-firebase/firestore';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-
-// firestore()
-//   .collection('users')
-//   .get()
-//   .then(querySnapshot => {
-//     console.log('Total users: ', querySnapshot.size);
-
-//     querySnapshot.forEach(documentSnapshot => {
-//       console.log('User ID: ', documentSnapshot.id, documentSnapshot.data());
-//     });
-//   });
-
-// const Stack = createStackNavigator();
-
-// function ScreenA() {
-//   return (
-//     <View>
-//       <Text>
-//         ScreenA
-//       </Text>
-//     </View>
-//   );
-// }
-
-// function App () {
-//   return (
-//     // <SafeAreaView>
-//     //   <FontAwesome5 name={'comments'} size={100} color='red'/>
-//     //   <Text>hello world3</Text>
-//     // </SafeAreaView>
-//     <NavigationContainer>
-//       <Stack.Navigator>
-//         <Stack.Screen 
-//           name="Screen_A"
-//           component={ScreenA}
-//         />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-// });
-
-// export default App;
-
 import React , {useCallback, useEffect, useRef, useState} from 'react';
 import {
   Pressable,
@@ -116,6 +23,7 @@ import UpdateDetail from './screens/UpdateDetail'
 import PutIngredient from './screens/PutIngredient'
 import Profile from './screens/Profile'
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+import EditProfile from './screens/EditProfile'
 // import Login from './screens/Login'
 
 const Stack = createStackNavigator();
@@ -174,6 +82,10 @@ function App(){
         <Stack.Screen
           name="UpdateDetail"
           component={UpdateDetail}
+        />
+        <Stack.Screen
+          name="EditProfile"
+          component={EditProfile}
         />
       </Stack.Navigator>
     </NavigationContainer>
